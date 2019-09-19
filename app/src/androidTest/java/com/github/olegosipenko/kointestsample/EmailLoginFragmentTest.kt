bbfb1bfc6f85764b2ecce2e6b499f478ae36021d
Oleg Osipenko <oleg.s.osipenko@gmail.com>
2019-09-19 10:42:02 +0200

    Updated dependencies

--- app/src/androidTest/java/com/github/olegosipenko/kointestsample/EmailLoginFragmentTest.kt
+++ app/src/androidTest/java/com/github/olegosipenko/kointestsample/EmailLoginFragmentTest.kt
@@ -1,9 +1,5 @@
 package com.github.olegosipenko.kointestsample
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.agoda.kakao.edit.KEditText
 import com.agoda.kakao.screen.Screen
