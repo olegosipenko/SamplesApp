c40c3ad20df5154966067f47263a5e52958f534e
Oleg Osipenko <oleg.s.osipenko@gmail.com>
2020-10-29 01:53:25 +0100

    Updated depdendencies

--- app/src/androidTest/java/com/github/olegosipenko/kointestsample/bootstrap/KoinTestApp.kt
+++ app/src/androidTest/java/com/github/olegosipenko/kointestsample/bootstrap/KoinTestApp.kt
@@ -5,13 +5,14 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.koin.core.module.Module
 
 class KoinTestApp: Application() {
   override fun onCreate() {
     super.onCreate()
     startKoin {
-      androidLogger()
+      androidLogger(Level.ERROR) // https://github.com/InsertKoinIO/koin/issues/871
       androidContext(this@KoinTestApp)
       modules(emptyList())
     }
--- app/src/main/java/com/github/olegosipenko/kointestsample/KoinApp.kt
+++ app/src/main/java/com/github/olegosipenko/kointestsample/KoinApp.kt
@@ -4,12 +4,13 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class KoinApp: Application() {
   override fun onCreate() {
     super.onCreate()
     startKoin {
-      androidLogger()
+      androidLogger(Level.ERROR) // https://github.com/InsertKoinIO/koin/issues/871
       androidContext(this@KoinApp)
       modules(
         listOf(
