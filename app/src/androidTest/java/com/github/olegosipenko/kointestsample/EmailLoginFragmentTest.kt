e5be257b53254a0e415f32c50b74a9f90d3c28c6
olegosipenko <oleg.s.osipenko@gmail.com>
2023-05-12 12:29:12 +0500

    Updated to dagger-hilt

--- app/src/androidTest/java/com/github/olegosipenko/kointestsample/EmailLoginFragmentTest.kt
+++ app/src/androidTest/java/com/github/olegosipenko/kointestsample/EmailLoginFragmentTest.kt
@@ -1,48 +1,62 @@
 package com.github.olegosipenko.kointestsample
 
+import android.util.Log
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.agoda.kakao.edit.KEditText
-import com.agoda.kakao.screen.Screen
-import com.agoda.kakao.screen.Screen.Companion.onScreen
-import com.agoda.kakao.text.KButton
-import com.github.olegosipenko.kointestsample.bootstrap.createRule
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
+import io.github.kakaocup.compose.node.element.KNode
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.dsl.module
 
+@HiltAndroidTest
 @RunWith(AndroidJUnit4ClassRunner::class)
 class EmailLoginFragmentTest {
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
 
-  private val fragmentViewModel: EmailLoginFragmentViewModel =
-    mockk(relaxed = true)
-  private val fragment = EmailLoginFragment()
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-  @get:Rule
-  val fragmentRule = createRule(fragment, module {
-    single(override = true) {
-      fragmentViewModel
+    @BindValue
+    @JvmField
+    val fragmentViewModel: EmailLoginFragmentViewModel = mockk(relaxed = true)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
     }
-  })
 
-  @Test
-  fun testBasicInvocation() {
-    onScreen<EmailLoginForm> {
-      emailField { typeText(EMAIL) }
-      passwordField { typeText(PASSWORD) }
-      loginButton.click()
-      verify {
-        fragmentViewModel.loginWithCredentials(EMAIL, PASSWORD)
-      }
+    @Test
+    fun testBasicInvocation() {
+        onComposeScreen<EmailLoginForm>(composeTestRule) {
+            emailField { performTextInput(EMAIL) }
+            passwordField { performTextInput(PASSWORD) }
+            loginButton { performClick() }
+
+            verify {
+                fragmentViewModel.loginWithCredentials(EMAIL, PASSWORD)
+            }
+        }
     }
-  }
-  class EmailLoginForm: Screen<EmailLoginForm>() {
-    val emailField = KEditText { withId(R.id.textFieldEmail) }
-    val passwordField = KEditText { withId(R.id.textFieldPassword) }
-    val loginButton = KButton { withId(R.id.buttonLogin) }
-  }
+
+    class EmailLoginForm(
+        semanticsProvider: SemanticsNodeInteractionsProvider
+    ) : ComposeScreen<EmailLoginForm>(semanticsProvider) {
+        val emailField: KNode = child { hasTestTag("email-field") }
+        val passwordField: KNode = child { hasTestTag("password-field") }
+        val loginButton: KNode = child { hasTestTag("button") }
+    }
 }
 
 private const val EMAIL = "some@email.com"
--- app/src/androidTest/java/com/github/olegosipenko/kointestsample/bootstrap/KoinTestRunner.kt
+++ app/src/androidTest/java/com/github/olegosipenko/kointestsample/bootstrap/KoinTestRunner.kt
@@ -3,13 +3,14 @@ package com.github.olegosipenko.kointestsample.bootstrap
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
 
 class KoinTestRunner: AndroidJUnitRunner() {
   override fun newApplication(
     cl: ClassLoader?, className: String?, context: Context?
   ): Application {
     return super.newApplication(
-      cl, KoinTestApp::class.java.name, context
+      cl, HiltTestApplication::class.java.name, context
     )
   }
 }
